279bcbb180c5534283281a5ccaee82889004605e
Charles Dennis <charlesdennis2009@hotmail.com>
2024-11-21 16:47:11 -0300

    adicionando eventos do jogo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -7,14 +7,49 @@ const state = {
 
     },
 
-    value: {},
+    values: {
+        timerId: null,
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+
+    },
 };
 
-function addListenerhitbox(){
-    state.view.squares.forEach((squere) =>)
+function moveEnemy(){
+    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
 
 }
 
-function init(){}
+function randomSquare(){
+    state.view.squares.forEach((square)=> {
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random()*9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+}
+
+function addListenerhitbox(){
+    state.view.squares.forEach((square) =>{
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.values.hitPosition){
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+            }
+        });
+
+       
+    });
+
+}
+
+function init(){
+    moveEnemy();
+    addListenerhitbox();
+}
 
 init();
