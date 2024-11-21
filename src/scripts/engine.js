28ac6e3593e3892caaf096a9da5d2e891edc1aaa
Charles Dennis <charlesdennis2009@hotmail.com>
2024-11-21 16:04:33 -0300

    Iniciando o JS

---
+++ src/scripts/engine.js
@@ -0,0 +1,20 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+
+    },
+
+    value: {},
+};
+
+function addListenerhitbox(){
+    state.view.squares.forEach((squere) =>)
+
+}
+
+function init(){}
+
+init();
